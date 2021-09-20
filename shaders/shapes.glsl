7f3502ed6dc0a128d103101380d2b91d46408639
Stefano Andriolo <stefano.andriolo@hotminds.it>
2021-09-20 19:37:06 +0200

    Add elements to the shapes shaders

--- shaders/shapes.glsl
+++ shaders/shapes.glsl
@@ -1,6 +1,7 @@
 precision mediump float;
 
 uniform vec2 u_resolution;
+uniform float u_time;
 
 float rect(vec2 orig, vec2 size, vec2 st) {
     vec2 bl = step(vec2(orig.x, orig.y), st);
@@ -19,11 +20,19 @@ void main() {
     // top right borders
     vec2 tr = smoothstep(vec2(0.1, 0.03), vec2(0.12, 0.05), 1.0 - st);
 
+    float boxColor = 
+        // (cos(u_time) + 1.0) * 0.5;
+        1.0;
+
     // color = vec3(bl.x * bl.y * tr.x * tr.y);    // equivalent to left && bottom && right && top
-    color = vec3(rect(vec2(0.0), vec2(0.5), st));
+    color = vec3(rect(vec2(0.0), vec2(0.5), st)) * vec3(boxColor);
     color += vec3(rect(vec2(0.35, 0.75), vec2(0.25), st));
-    color += vec3(rect(vec2(0.55, 0.0), vec2(0.45, 0.70), st));
+    color += vec3(rect(vec2(0.55, 0.30), vec2(0.45, 0.40), st));
     color += vec3(rect(vec2(0.0, 0.55), vec2(0.20, 0.45), st)) * vec3(0.2, 0.4, 0.8);
+    color += vec3(rect(vec2(0.55, 0.0), vec2(0.45, 0.25), st));
+    color += vec3(rect(vec2(0.25, 0.55), vec2(0.25, 0.15), st));
+    color += vec3(rect(vec2(0.25, 0.75), vec2(0.05, 0.25), st));
+    color += vec3(rect(vec2(0.65, 0.75), vec2(0.35, 0.25), st)) * vec3(0.9686, 0.3216, 0.3216);
 
     gl_FragColor = vec4(color,  1.0);
 }
